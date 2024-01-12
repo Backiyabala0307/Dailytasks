51788e1d63910767c264adbed3bed29e2f255e41
Kavyabala0307 <kavyabala3799@gmail.com>
2024-01-12 12:01:03 +0530

    day15 task updated

--- day15task/form-table/script.js
+++ day15task/form-table/script.js
@@ -2,12 +2,12 @@ let button=document.getElementById('submit');
 button.addEventListener('click',handler);
 function handler(){
     let first = document.getElementById('firstname');
-    let tr = document.createElement('tr');
-    let td = document.createElement('td');
+    let tr = document.createElement(tr);
+    let td = document.createElement(td);
 
     tbody.appendChild(tr);
     tr.appendChild(td);
-    td.textContent= first.target.value;
+    td.textContent= first.textContent;
 }
 
 
