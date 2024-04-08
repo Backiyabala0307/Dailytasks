072f6c435939d23882d92a4da79a006128d8c10a
Kavyabala0307 <kavyabala3799@gmail.com>
2024-04-08 12:56:19 +0530

    day23task was modified

--- day23task/reactday23task/src/App.jsx
+++ day23task/reactday23task/src/App.jsx
@@ -1,33 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ProductList from './ProductList'
-import ShoppingCart from './ShoppingCart'
+import React, { useState } from 'react'
+import ProductItems from './ProductItems'
+import  './App.css'
 
 function App() {
+
+  const [cartItem, setCartItem] = useState([]);
   const [count, setCount] = useState(0);
+
+  const AddToCart = (producttoadd) => {
+    if (cartItem.some(product => product.id === producttoadd.id)) {
+        return setCartItem(prevItems => [...prevItems]);
+    }
+    
+    else if (!cartItem.includes(producttoadd)) {
+      setCartItem(prevItems => [...prevItems,producttoadd]);
+      setCount(count => count + 1);
+    }
+  };
+
+  // console.log(cartItem);
+  // console.log(count);
   
-  const handleAddToCart = () => {
-    setCount(count => count + 1);
-}
-  const handleRemoveFromCart = () => {
-    setCount(count => count - 1);
 
-  }
-
+  const RemoveFromCart = (producttoremove) => {
+    if (cartItem.some(product => product.id === producttoremove.id)) {
+      setCartItem(cartItem.filter(item=>item.id !== producttoremove.id));
+      setCount(count => count - 1);
+    }
+  };
+  
   return (
-    <div className="App">
-      <h1>Shopping Cart</h1>
-      <button class="btn btn-outline-dark" type="submit">
-        Cart
-        <span class="badge bg-dark text-white ms-1 rounded-pill">{count}</span>
-      </button>
-  
-      <ProductList onAddToCart={handleAddToCart} onRemoveFromCart={handleRemoveFromCart} />
-      {/* <ShoppingCart cartItems={cartItems} onRemoveFromCart={handleRemoveFromCart} /> */}
-      
+    <div className='App'>
+      {/* <div className='d-flex'> */}
+          <h1>Shopping Cart</h1>
+          <button className=" btn btn-info btn-outline-dark" type="submit">
+            Cart
+            <span className="badge bg-dark text-white ms-1 rounded-pill">{count }</span>
+          </button>
+      {/* </div> */}
+
+       <ProductItems AddToCart={AddToCart} RemoveFromCart={RemoveFromCart}/>
     </div>
   )
-}
+  }
 
 export default App
--- day23task/reactday23task/src/ProductItems.jsx
+++ day23task/reactday23task/src/ProductItems.jsx
@@ -1,33 +1,72 @@
 import React from 'react'
 import './ProductItems.css'
 
-function ProductItems(props) {
+function ProductItems({AddToCart ,RemoveFromCart}) {
+    
+    const products = [{
+        id:0,
+        title: "ATOMIC HABITS",
+        author: "Jhon James",
+        price: "$100"
+    },
+    {   
+        id:1,
+        title: "Visualization",
+        author: "Goldsmith",
+        price: "$40"
+    },
+    {
+        id:2,
+        title: "Victory",
+        author: "George Fernantas",
+        price: "$68"
+    },
+    {
+        id:3,
+        title: "THE SECRET",
+        author: "Carley",
+        price: "$230"
+    },
+    {
+        id:4,
+        title: "THE PARK",
+        author: "James",
+        price: "$50"
+    },
+    {
+        id:5,
+        title: "The Heaven in the Hell",
+        author: "Roynald rein",
+        price: "$210"
+        }];
+    
   return (
-    <div className='ProductItems'>
-          <div class="col">
-              <div class="card bg-info-subtle">
-                  <div class="card-body p-4">
-                      <div class="text-center">
-                          <h5 class="fw-bolder">{ props.title }</h5>
-                          <p class="fw-bolder">{ props.author }</p>
-                          <div class="d-flex justify-content-center small text-warning mb-2">
-                              <div class="bi-star-fill">⭐</div>
-                              <div class="bi-star-fill">⭐</div>
-                              <div class="bi-star-fill">⭐</div>
-                              <div class="bi-star-fill">⭐</div>
-                              <div class="bi-star-fill">⭐</div>
+      <div className='ProductItems'>
+          <h2>Product List</h2>
+          <ul className='row gy-3'>
+              {products.map((product) =>(
+                  <li className='col-sm-6 col-lg-4' key={product.id}>
+                      <div className="card">
+                          
+                            <div className="card-body p-4">
+                                <div className="text-center">
+                                <h5 className="fw-bolder">{ product.title }</h5>
+                                <h6 className="fw-bolder">{product.author }</h6>
+                                    {product.price }
+                                </div>
+                            </div>
+                          <div className="card-footer d-inline-flex p-4 pt-0 border-top-0 bg-transparent">
+                                <div className="text-center row">
+                                  <button className='ms-2 col btn btn-success btn-outline-dark mt-auto' onClick={() => AddToCart(product)}>Add to cart</button>
+                                  <button className='ms-2 col btn btn-danger btn-outline-dark mt-auto' onClick={() => RemoveFromCart(product)}>Rmove From cart</button>
+
+                                </div>
+                            </div>
+
+                           
                           </div>
-                          <span class="text">{props.price}</span>
-                      </div>
-                 </div>
-                  <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                      <div class="text-center row ">
-                          <a class="ms-2 col btn btn-outline-dark mt-auto" onClick={()=>props.onAddToCart()}>Add to cart</a>
-                          <a class="ms-2 col btn btn-outline-dark mt-auto" onClick={() => props.onRemoveFromCart()}>Remove From cart</a>
-                      </div>
-                  </div>
-              </div>
-           </div>  
+                  </li>))}
+              </ul>
     </div>
   )
 }
